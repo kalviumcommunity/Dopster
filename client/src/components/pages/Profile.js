df256218ef26265c1020a3f52c72341116644838
Ayush Kumar <ayushkumar28082004@gmail.com>
2023-04-03 21:50:15 +0530

    implemented paypal payment gateway

--- client/src/components/pages/Profile.js
+++ client/src/components/pages/Profile.js
@@ -1,9 +1,15 @@
 import React from 'react'
-
+import { useContext } from 'react';
+import { UserContext } from '../../App';
 const Profile = () => {
+  const { state } = useContext(UserContext);
+  console.log(state)
   return (
     <div>
-      <h1>Profile</h1>
+      <img src={state.pic} alt="Profile" />
+      <h2>{state.name}</h2>
+      <h3>{state.email}</h3>
+      <h3>{state.dopeCredits}</h3>
     </div>
   )
 }
