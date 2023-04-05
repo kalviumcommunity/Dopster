ef46587296a33fbc879f81f14748733d2676787b
Praduman03 <praduman03k@gmail.com>
2023-04-05 14:47:32 +0530

    fixed minor bugs

--- server/models/postModel.js
+++ server/models/postModel.js
@@ -1,30 +1,37 @@
-const mongoose = require('mongoose')
-const id=mongoose.Schema.Types.ObjectId
+const mongoose = require("mongoose");
+const id = mongoose.Schema.Types.ObjectId;
 
+const postSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    body: {
+      type: String,
+      default: true,
+    },
+    link: {
+      type: String,
+      required: true,
+    },
+    likes: [{ type: id, ref: "User" }],
+    comments: [
+      {
+        text: String,
+        postedBy: { type: id, ref: "User" },
+      },
+    ],
+    photo: {
+      type: String,
+      required: true,
+    },
+    postedBy: {
+      type: id,
+      ref: "User",
+    },
+  },
+  { timestamps: true }
+);
 
-
-const postSchema = new mongoose.Schema({
-    title:{
-        type:String,
-        required:true
-    },
-    body:{
-        type:String,
-        default:true
-    },
-    link:{
-        type:String,
-        required:true
-    },
-    likes:[{type:id,ref:"User"}],
-    photo:{
-        type:String,
-        required:true
-    },
-    postedBy:{
-        type:id,
-        ref:"User"
-    }
-},{timestamps:true})
-
-mongoose.model("Post",postSchema)
+mongoose.model("Post", postSchema);
