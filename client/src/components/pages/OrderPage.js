cf5852294e7df6c3e6da4076b153f26d87a46e62
Ayush Kumar <ayush.kumar@kalvium.community>
2023-04-12 21:33:43 +0530

    changes in responsiveness

--- client/src/components/pages/OrderPage.js
+++ client/src/components/pages/OrderPage.js
@@ -1,21 +1,63 @@
-import React, { useEffect, useState } from 'react'
-import Paypalbutton from './Paypalbutton'
-
+import React, { useEffect, useState } from "react";
+import Paypalbutton from "./Paypalbutton";
+import ResponsiveAppBar from "./Navbar";
+import Footer from "./Footer";
 const OrderPage = () => {
-    const product ={
-        description:"Dopster Premium",
-        price:1
-    }
+  const product = {
+    description: "Dopster Premium",
+    price: 1,
+  };
+  const abcd = async ()=>{
+    const resp = await fetch(process.env.REACT_APP_API+"/cashfree", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: "Bearer " + localStorage.getItem("jwt"),
+        },
+        body: JSON.stringify({
+          amount:50
+        }),
+      })
+      const postjson = await resp.json()
+      console.log(postjson)
+      const paymentSessionId = postjson;
+      const cashfree = new window.Cashfree(paymentSessionId);
+    // //   console.log(cashfree);
+      return cashfree.redirect()
+
+}
   return (
     <>
-    <div>
-       
-    </div>
-    <div className='paypal-button-container' >
-        <Paypalbutton product={product} />
-    </div>
-    </>
-  )
-}
+      <ResponsiveAppBar />
+      <div>
 
-export default OrderPage
+     
+      <div>
+        <table>
+          <tr>
+            <th>Amount</th>
+            <th>Status</th>
+            <th>Order Id</th>
+          </tr>
+          <tr>
+            <td>Dopster Premium</td>
+            <td>INR 50</td>
+            <td> 1</td>
+          </tr>
+        </table>
+      </div>
+      <div className="paypal-button-container">
+        <Paypalbutton product={product} />
+      </div>
+      <div>
+    <button onClick={abcd}  className="App">
+        Cashfree Payments
+    </button>
+    </div>
+    </div>
+      <Footer />
+    </>
+  );
+};
+
+export default OrderPage;
